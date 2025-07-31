1e9970236249d6dcd9e88f4314dd6b88a26136b4
이미진 <imagine@imijin-ui-Mac-Studio.local>
2025-07-31 14:50:25 +0900

    유튜브 댓글 이미지 생성기 변경

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 import io
 
 st.set_page_config(layout="centered")
-st.title("💬 유튜브 글 이미지 자동 생성기")
+st.title("💬 유튜브 댓글 이미지 생성기")
 
 # 입력
 bg_color = st.radio("배경 색상 선택", ("흰색", "검은색"))
